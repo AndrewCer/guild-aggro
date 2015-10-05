3a78c9ec2c47775107a7f961424fc609b2cdc6ce
Andrew Cer <cer.andrew@gmail.com>
2015-10-05 15:37:06 -0600

    added more seed text to post messages and changed font sizes

--- public/js/controller.js
+++ public/js/controller.js
@@ -311,19 +311,19 @@ app.controller('BlueController', ['$scope', function ($scope) {
   $scope.posts = [
     {
       title: 'guild raid tonight',
-      message: 'message Tom for all the fun and exciting info',
+      message: 'PBR&B stumptown trust fund put a bird on it Intelligentsia lumbersexual. Master cleanse vinyl disrupt, lomo Pinterest kogi cornhole hella. Literally wayfarers vinyl mixtape. Blog Blue Bottle lumbersexual, wayfarers beard cray gentrify lo-fi kitsch trust fund health goth quinoa roof party brunch aesthetic. Fap organic Vice, Thundercats cliche street art readymade jean shorts fixie four loko selvage tofu. XOXO kitsch Intelligentsia hoodie. Locavore Portland literally authentic, Bushwick wolf four dollar toast.',
       poster: 'Andrew',
       comments: ['yea','sweet']
     },
     {
       title: 'We won',
-      message: 'such and such raid is now our bitch',
+      message: 'PBR&B stumptown trust fund put a bird on it Intelligentsia lumbersexual. Master cleanse vinyl disrupt, lomo Pinterest kogi cornhole hella. Literally wayfarers vinyl mixtape. Blog Blue Bottle lumbersexual, wayfarers beard cray gentrify lo-fi kitsch trust fund health goth quinoa roof party brunch aesthetic. Fap organic Vice, Thundercats cliche street art readymade jean shorts fixie four loko selvage tofu. XOXO kitsch Intelligentsia hoodie. Locavore Portland literally authentic, Bushwick wolf four dollar toast.',
       poster: 'John',
       comments: ['yea','sweet']
     },
     {
       title: 'first post woo!',
-      message: 'I cant believe we did it!',
+      message: 'PBR&B stumptown trust fund put a bird on it Intelligentsia lumbersexual. Master cleanse vinyl disrupt, lomo Pinterest kogi cornhole hella. Literally wayfarers vinyl mixtape. Blog Blue Bottle lumbersexual, wayfarers beard cray gentrify lo-fi kitsch trust fund health goth quinoa roof party brunch aesthetic. Fap organic Vice, Thundercats cliche street art readymade jean shorts fixie four loko selvage tofu. XOXO kitsch Intelligentsia hoodie. Locavore Portland literally authentic, Bushwick wolf four dollar toast.',
       poster: 'Steve',
       comments: ['yea','sweet']
     }
